047ea762dab3f2dcde9c9b93cda609a3ceba35c3
ConnorBonn <connorb809@lpsk12.org>
2023-11-17 11:25:00 -0700

    add function count and started print preorder

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,2 +1,73 @@
-public class BinarySearchTree {
+import java.util.Stack;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+    private TreeNode<E> root;
+    private int countNode = 0;
+
+    void add(E value) {
+        //makes new node
+        TreeNode<E> addedElement = new TreeNode<E>(value, null, null);
+        if (root == null) {
+            root = addedElement;
+        } else {
+            TreeNode<E> temp = root;
+            while (temp.getRightChild() != null && temp.getLeftChild() != null) {
+                if (temp.getValue().compareTo(addedElement.getValue()) <= 0) {
+                    if(temp.getLeftChild() == null){
+                        temp.setLeftChild(addedElement);
+                    }
+                        else{
+                            temp = temp.getLeftChild();
+                    }
+                    if(temp.getRightChild() == null){
+                        temp.setRightChild(addedElement);
+                    }
+                    else{
+                        temp = temp.getRightChild();
+                    }
+                }
+            }
+        }
+        countNode++;
+    }
+
+    boolean contains(E value) {
+
+    }
+
+    int countNodes() {
+        return countNode;
+    }
+
+    int countLeafNodes() {
+
+    }
+
+    int getHeight() {
+
+    }
+
+    void printInorder() {
+
+    }
+
+    void printPreorder() {
+        Stack<TreeNode<E>> stack = new Stack<>();
+        TreeNode<E> current = root;
+
+        while(current != null || !stack.isEmpty()){
+            while(current != null){
+                System.out.println();
+            }
+        }
+    }
+
+    void printPostorder() {
+
+    }
+
+    E delete(E value) {
+        countNode--;
+        return value;
+    }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -11,11 +11,12 @@ public class TreeNode<E extends Comparable<E>> {
 
     /**
      * Creates a new TreeNode
-     * @param val - the value held at the node
-     * @param left - the left child
+     *
+     * @param val   - the value held at the node
+     * @param left  - the left child
      * @param right - the right child
      */
-    public TreeNode(E val, TreeNode<E> left, TreeNode<E> right){
+    public TreeNode(E val, TreeNode<E> left, TreeNode<E> right) {
         value = val;
         leftChild = left;
         rightChild = right;
@@ -23,49 +24,55 @@ public class TreeNode<E extends Comparable<E>> {
 
     /**
      * Retrieves the left child of this node
+     *
      * @return the left child of the node
      */
-    public TreeNode<E> getLeftChild(){
+    public TreeNode<E> getLeftChild() {
         return leftChild;
     }
 
     /**
      * Retrieves the right child of this node
+     *
      * @return the right child of this node
      */
-    public TreeNode<E> getRightChild(){
+    public TreeNode<E> getRightChild() {
         return rightChild;
     }
 
     /**
      * Retrieves the value of this node
+     *
      * @return the value contained at this node
      */
-    public E getValue(){
+    public E getValue() {
         return value;
     }
 
     /**
      * Sets the left child to the node given
+     *
      * @param left the new left child node
      */
-    public void setLeftChild(TreeNode<E> left){
+    public void setLeftChild(TreeNode<E> left) {
         leftChild = left;
     }
 
     /**
      * Sets the right child to the node given
+     *
      * @param right the new right child node
      */
-    public void setRightChild(TreeNode<E> right){
+    public void setRightChild(TreeNode<E> right) {
         rightChild = right;
     }
 
     /**
      * Sets the value to the value given
+     *
      * @param val the new value of the node
      */
-    public void setValue(E val){
+    public void setValue(E val) {
         value = val;
     }
 
