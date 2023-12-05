0312ba134edf7b267ec601fe185d96ee12532a4e
ConnorBonn <connorb809@lpsk12.org>
2023-12-05 11:28:41 -0700

    Added contains Added countLeafNodes fixed add

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -5,83 +5,115 @@ public class BinarySearchTree<E extends Comparable<E>> {
     private TreeNode<E> root;
     private int countLeaf = 0;
     private int countNode = 0;
+    private int height = 0;
 
     void add(E value) {
         //makes new node
         TreeNode<E> addedElement = new TreeNode<E>(value, null, null);
+        //checks if the root exists
         if (root == null) {
+            //changes the element to the root
             root = addedElement;
+            countLeaf = 1;
         } else {
+            //Creates a new node
             TreeNode<E> temp = root;
             while (true) {
+                //checks if the new node is less than the current
                 if (temp.getValue().compareTo(addedElement.getValue()) >= 0) {
+                   //checks if the left child exists
                     if (temp.getLeftChild() == null) {
+                        //sets new node as left child
                         temp.setLeftChild(addedElement);
+                        //adds the new leaf to the count
+                        countLeaf++;
+                        //checks if the left child is the only child
+                        if(temp.getLeftChild() != null && temp.getRightChild() == null){
+                            //removes a leaf from the count
+                            countLeaf--;
+                        }
                         break;
+                        //
                     } else {
+                        //moves down the tree
                         temp = temp.getLeftChild();
                     }
+                    //checks if the new node is more than the current
                 } else if (temp.getValue().compareTo((addedElement.getValue())) < 0) {
+                    //checks if the right child exists
                     if (temp.getRightChild() == null) {
+                        //sets the new node as right child
                         temp.setRightChild(addedElement);
+                        //add the new leaf to the count
+                        countLeaf++;
+                        //checks if the right child is the only child
+                        if(temp.getRightChild() != null && temp.getLeftChild() == null){
+                            //removes a leaf from the count
+                            countLeaf--;
+                        }
                         break;
                     } else {
+                        //moves to the next node
                         temp = temp.getRightChild();
                     }
                 }
             }
         }
+        //increases the number of nodes
         countNode++;
     }
 
-    /* boolean contains(E value) {
-
+    boolean contains(E value) {
+        //creates a stack
+        Stack<TreeNode<E>> stack = new Stack<>();
+        //makes a node for tracing
+        TreeNode<E> current = root;
+        //loops while current isn't null and the stack isn't empty
+        while (current != null || !stack.isEmpty()) {
+            //loops while current isn't null
+            while (current != null) {
+                //checks if current is equal to the value
+                if(current.getValue() == value){
+                    return true;
+                }
+                //pushes right children into the stack
+                stack.push(current.getRightChild());
+                //traces left subtree
+                current = current.getLeftChild();
+            }
+            if (!stack.isEmpty()) {
+                //makes the old right child equal to the current
+                current = stack.pop();
+            }
+        }
+        return false;
     }
-*/
+
     int countNodes() {
+        //returns the total number of nodes
         return countNode;
     }
 
     int countLeafNodes() {
-        Stack<TreeNode<E>> stack = new Stack<>();
-        TreeNode<E> current = root;
-        while (current != null || !stack.isEmpty()) {
-            while (current != null) {
-                if (current.getRightChild() == null && current.getLeftChild() == null) {
-                    countLeaf++;
-                } else {
-                    stack.push(current.getRightChild());
-                    current = current.getLeftChild();
-                }
-            }
-            if (!stack.isEmpty()) {
-                current = stack.pop();
-            }
-        }
+        //returns the number of leaf nodes
         return countLeaf;
     }
 
-    /*
-        int getHeight() {
 
+   /*     int getHeight() {
+            return height;
         }
-    */
+/*
     void printInorder() {
         Stack<TreeNode<E>> stack = new Stack<>();
         TreeNode<E> parent = root;
-        TreeNode<E> current = root;
-        while (parent.getLeftChild() != null) {
+        TreeNode<E> current = parent.getLeftChild();
+        while(){
 
-            if (current.getRightChild() != null && current.getLeftChild() == null) {
-                System.out.println(current);
-                parent = current;
-                current = parent.getRightChild();
-            } else if (current.getRightChild() == null && current.getLeftChild() == null) {
-                System.out.println(current);
-                System.out.println(parent);
-            }
         }
-    }
+
+    }*/
+
 
     public void printPreorder() {
         Stack<TreeNode<E>> stack = new Stack<>();
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
     public static void main(String [] args){
-        BinarySearchTree bruh = new BinarySearchTree();
-        bruh.add(5);
-        bruh.add(10);
-        bruh.add(3);
-        bruh.add(8);
-        bruh.add(2);
-        bruh.add(9);
-        bruh.add(7);
-        bruh.add(5);
-        bruh.add(4);
-        bruh.add(1);
-        bruh.add(6);
-        bruh.printPreorder();
-        System.out.println(bruh.countNodes());
-        System.out.println(bruh.countLeafNodes());
+        BinarySearchTree tree = new BinarySearchTree();
+
+        System.out.println(tree.contains(10));
+        System.out.println(tree.countNodes());
+        System.out.println(tree.countLeafNodes());
     }
 }
