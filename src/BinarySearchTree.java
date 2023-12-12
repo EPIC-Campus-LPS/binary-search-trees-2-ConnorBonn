6bd611f64f728ed38f22ca4bf4293e5a2a4d47f3
ConnorBonn <connorb809@lpsk12.org>
2023-12-12 11:18:10 -0700

    Delete broke everything else done

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -5,7 +5,6 @@ public class BinarySearchTree<E extends Comparable<E>> {
     private TreeNode<E> root;
     private int countLeaf = 0;
     private int countNode = 0;
-    private int height = 0;
 
     void add(E value) {
         //makes new node
@@ -21,17 +20,10 @@ public class BinarySearchTree<E extends Comparable<E>> {
             while (true) {
                 //checks if the new node is less than the current
                 if (temp.getValue().compareTo(addedElement.getValue()) >= 0) {
-                   //checks if the left child exists
+                    //checks if the left child exists
                     if (temp.getLeftChild() == null) {
                         //sets new node as left child
                         temp.setLeftChild(addedElement);
-                        //adds the new leaf to the count
-                        countLeaf++;
-                        //checks if the left child is the only child
-                        if(temp.getLeftChild() != null && temp.getRightChild() == null){
-                            //removes a leaf from the count
-                            countLeaf--;
-                        }
                         break;
                         //
                     } else {
@@ -44,13 +36,6 @@ public class BinarySearchTree<E extends Comparable<E>> {
                     if (temp.getRightChild() == null) {
                         //sets the new node as right child
                         temp.setRightChild(addedElement);
-                        //add the new leaf to the count
-                        countLeaf++;
-                        //checks if the right child is the only child
-                        if(temp.getRightChild() != null && temp.getLeftChild() == null){
-                            //removes a leaf from the count
-                            countLeaf--;
-                        }
                         break;
                     } else {
                         //moves to the next node
@@ -73,7 +58,7 @@ public class BinarySearchTree<E extends Comparable<E>> {
             //loops while current isn't null
             while (current != null) {
                 //checks if current is equal to the value
-                if(current.getValue() == value){
+                if (current.getValue() == value) {
                     return true;
                 }
                 //pushes right children into the stack
@@ -96,46 +81,150 @@ public class BinarySearchTree<E extends Comparable<E>> {
 
     int countLeafNodes() {
         //returns the number of leaf nodes
+        Stack<TreeNode<E>> stack = new Stack<>();
+        //makes a node for tracing
+        TreeNode<E> current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                //checks if the node is a leaf
+                if (current.getLeftChild() == null && current.getRightChild() == null) {
+                    countLeaf++;
+                }
+                //pushes currents right child to the stack
+                stack.push(current.getRightChild());
+                //move current to the left
+                current = current.getLeftChild();
+            }
+            if (!stack.isEmpty()) {
+                //sets current to a node that had been pushed into the stack
+                current = stack.pop();
+            }
+        }
         return countLeaf;
     }
 
 
-   /*     int getHeight() {
-            return height;
+    public int getHeight() {
+        return getHeight(root);
+    }
+
+    private int getHeight(TreeNode<E> node) {
+        if (node == null) {
+            return -1;
         }
-/*
-    void printInorder() {
-        Stack<TreeNode<E>> stack = new Stack<>();
-        TreeNode<E> parent = root;
-        TreeNode<E> current = parent.getLeftChild();
-        while(){
-
+        if (node.getLeftChild() == null && node.getRightChild() == null) {
+            return 0;
         }
+        return 1 + Math.max(getHeight(node.getLeftChild()), getHeight(node.getRightChild()));
+    }
 
-    }*/
+    public void printInorder() {
+        printInorder(root);
+    }
 
+    private void printInorder(TreeNode<E> root) {
+        if (root == null)
+            return;
+        printInorder(root.getLeftChild());
+        System.out.println(root.getValue());
+        printInorder(root.getRightChild());
+    }
 
     public void printPreorder() {
-        Stack<TreeNode<E>> stack = new Stack<>();
-        TreeNode<E> current = root;
-        while (current != null || !stack.isEmpty()) {
-            while (current != null) {
-                System.out.println(current.getValue());
-                stack.push(current.getRightChild());
-                current = current.getLeftChild();
-            }
-            if (!stack.isEmpty()) {
-                current = stack.pop();
-            }
-        }
+        printPreorder(root);
     }
-  /*  void printPostorder() {
 
+    private void printPreorder(TreeNode<E> root) {
+        if (root == null)
+            return;
+        System.out.println(root.getValue());
+        printPreorder(root.getLeftChild());
+        printPreorder(root.getRightChild());
+    }
+
+    public void printPostorder() {
+        printPostorder(root);
+    }
+
+    private void printPostorder(TreeNode<E> root) {
+        if (root == null)
+            return;
+        printPostorder(root.getLeftChild());
+        printPostorder(root.getRightChild());
+        System.out.println(root.getValue());
     }
 
     E delete(E value) {
+        TreeNode<E> current = root;
+        if (!contains(value)) {
+            return null;
+        } else if (root.getValue() == value) {
+            if (root.getLeftChild() == null && root.getRightChild() != null) {
+                root = root.getRightChild();
+            } else {
+                TreeNode<E> rightTree = root.getRightChild();
+                root.setRightChild(null);
+                root = root.getLeftChild();
+                while (true) {
+                    if (current.getRightChild() == null) {
+                        //sets the new node as right child
+                        current.setRightChild(rightTree);
+                        break;
+                    } else {
+                        //moves to the next node
+                        current = current.getRightChild();
+                    }
+                }
+            }
+            if (current.getLeftChild() == null && current.getRightChild() == null) {
+                root = null;
+                return null;
+            }
+        }
+        else if(value != root.getValue()){
+            TreeNode<E> parent = root;
+            Stack<TreeNode<E>> stack = new Stack<>();
+            while (current != value || !stack.isEmpty()) {
+                while (current != value) {
+                    if(root.getValue().compareTo(value) >= 0){
+                        parent = root.getLeftChild();
+                    }
+                    else{
+                        parent = root.getRightChild();
+                    }
+                    current = parent.getLeftChild();
+                    if(current.getValue() == value){
+                        if(current.getLeftChild() == null && current.getRightChild() != null){
+                            parent.setLeftChild(current.getRightChild());
+                        }
+                        else if(current.getLeftChild() != null && current.getRightChild() == null){
+                            parent.setLeftChild(current.getLeftChild());
+                            current.getLeftChild().setRightChild(current.getRightChild());
+                            current = current.getLeftChild();
+                        }
+                        else if(current.getLeftChild() == null && current.getLeftChild() == null){
+                            current = null;
+                        }
+                    }
+                    //pushes currents right child to the stack
+                    stack.push(current.getRightChild());
+                    //moves to the left
+                    parent = parent.getLeftChild();
+                    if(parent.getLeftChild() == null){
+                        current = parent.getRightChild();
+                    }
+                    else {
+                        current = parent.getLeftChild();
+                    }
+                }
+                if (!stack.isEmpty()) {
+                    //sets current to a node that had been pushed into the stack
+                    current = stack.pop();
+                }
+            }
+        }
         countNode--;
         return value;
-    }*/
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-public class Main {
-    public static void main(String [] args){
-        BinarySearchTree tree = new BinarySearchTree();
+import java.io.*;
+import java.util.*;
 
-        System.out.println(tree.contains(10));
-        System.out.println(tree.countNodes());
-        System.out.println(tree.countLeafNodes());
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(4);
+        tree.add(2);
+        tree.add(1);
+        tree.add(3);
+        tree.add(6);
+        tree.add(5);
+        tree.add(7);
+        tree.delete(4);
+        tree.printPreorder();
     }
 }
